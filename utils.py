9b96fe81a95ab81305f4f29be8d70bfce93fd941
huyulan <huyulan@boe.com.cn>
2018-12-18 09:43:16 +0800

    add

--- utils.py
+++ utils.py
@@ -4,14 +4,30 @@
 # @Email   : huyulan@boe.com.cn
 # @File    : test.py
 # @Software: PyCharm
-list = []
-import os
-with open("D:/boe-data/项目申请/糖尿病/糖尿病数据 - 副本.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        split = line.split("\t", 8)
-        filename = os.path.basename(split[0])
-        ss= split[0].replace(filename,"")
-        list.append(ss)
-# v = frozenset(set)
-for i in set(list):
-    print(i)
+
+import pandas as pd
+
+
+def run_with_one_corpus_for_quora(self, file_path):
+    corpus = {}
+
+    df = pd.read_csv(file_path)
+
+    qid1 = list(df['qid1'])
+    qid2 = list(df['qid2'])
+    question1 = list(df['question1'])
+    question2 = list(df['question2'])
+    is_duplicate = list(df['is_duplicate'])
+
+    for x1, x2 in zip(qid1, question1):
+        corpus[x1] = x2
+    for x1, x2 in zip(qid2, question2):
+        corpus[x1] = x2
+
+    rel = [(label, qid1, qid2) for label, qid1, qid2 in zip(is_duplicate, qid1, qid2)]
+    return corpus, rel
+
+
+if __name__ == '__main__':
+    run_with_one_corpus_for_quora("")
+
